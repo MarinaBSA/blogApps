6d12d18543d9d534e77d096e63534143574bef19
Marina A <25265018+MarinaBSA@users.noreply.github.com>
2020-07-26 21:23:39 +0200

    finished first version of tapping app

---
+++ tapping_app/tapping_app_uikit/tapping_app_uikit/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  tapping_app_uikit
+//
+//  Created by Marina Beatriz Santana de Aguiar on 26.07.20.
+//  Copyright © 2020 Marina Beatriz Santana de Aguiar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
